aee5da1498e816a30689702a3a9fd6dc519cd4b9
Divya Mohan <43429739+Divya1173@users.noreply.github.com>
2021-05-16 14:42:24 +0530

    Test to verify AWS CodePipeline

--- src/main/java/com/AWS/CodeCommit/HomePage.java
+++ src/main/java/com/AWS/CodeCommit/HomePage.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomePage {
     @GetMapping("/")
 	public String welcome() {
-    	return "Welcome to My Demo Spring Boot project!";
+    	return "Welcome to My Demo Spring Boot project! This is now test to verify CodePipeline";
     }
 }
